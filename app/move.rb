777c058367b54a2c680870b40b6cd28d70ce389a
Freya <freya@stubkjaer.co.uk>
2021-06-03 12:16:16 -0700

    Outputs board state

--- app/move.rb
+++ app/move.rb
@@ -2,8 +2,8 @@
 # Valid moves are "up", "down", "left", or "right".
 # TODO: Use the information in board to decide your next move.
 def move(board)
-  puts board
-
+  # puts board
+  puts "Board State: " + board[:board]
   # Choose a random direction to move in
   possible_moves = ["up", "down", "left", "right"]
   move = possible_moves.sample
