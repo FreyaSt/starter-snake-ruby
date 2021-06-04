430773be37d8291c7cc503f89c46c049a9962cd1
Freya <freya@stubkjaer.co.uk>
2021-06-03 17:52:47 -0700

    Extracts targeting to new function

--- app/move.rb
+++ app/move.rb
@@ -11,6 +11,22 @@ def move(board)
   # possible_moves = ["up", "down", "left", "right"]
   head = [board[:you][:head][:x], board[:you][:head][:y]]
   
+  # puts "The current head location is #{head}"
+  # if head == [0, 0]
+  #   puts "Switching target to 10, 0"
+  #   $dest = [10, 0]
+  # elsif head == [10, 0]
+  #   puts "Switching target to 10, 10"
+  #   $dest = [10, 10]
+  # end
+  getDest(head)
+  move = moveTo($dest, head)
+  puts "MOVE: " + move
+  { "move": move }
+
+end
+
+def getDest(head)
   puts "The current head location is #{head}"
   if head == [0, 0]
     puts "Switching target to 10, 0"
@@ -19,11 +35,9 @@ def move(board)
     puts "Switching target to 10, 10"
     $dest = [10, 10]
   end
-  move = moveTo($dest, head)
-  puts "MOVE: " + move
-  { "move": move }
-
+  
 end
+
 
 def moveTo(dest, head)
   if head[0] > dest[0]
